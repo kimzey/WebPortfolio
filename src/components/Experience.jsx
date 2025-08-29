efb03edfca85385b7599065678c82e97bc22ceb8
kimzey <129323878+kimzey@users.noreply.github.com>
2025-08-29 22:55:47 +0700

    Use CDN React globals for runtime

---
+++ src/components/Experience.jsx
@@ -0,0 +1,18 @@
+import experiences from '../data/experiences.js';
+
+const Experience = () => (
+  <div>
+    <h1>ประสบการณ์</h1>
+    {experiences.map((exp, idx) => (
+      <div key={idx} className="experience">
+        <h2>{exp.company}</h2>
+        <p>{exp.role} - {exp.type}</p>
+        <p>{exp.period}</p>
+        <p>{exp.location}</p>
+        {exp.description && <p>{exp.description}</p>}
+      </div>
+    ))}
+  </div>
+);
+
+export default Experience;
